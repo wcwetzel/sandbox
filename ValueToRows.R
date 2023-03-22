bcf5b9f397ed76783d162d9f52085e5bf73222d5
Will Wetzel <wcwetzel@users.noreply.github.com>
2023-03-22 09:56:31 -0400

    solved value n to n rows with uncount function in tidyr

--- ValueToRows.R
+++ ValueToRows.R
@@ -3,14 +3,44 @@
 
 # This script gives a toy example of converting from one row per plot to one row
 # per insect. We often enter counts of insects per plot, but then we need to
-# work with a dataframe with one row per insect. So we need to use the value of
+# work with a data frame with one row per insect. So we need to use the value of
 # a column to add rows.
+# The solution uses the uncount function in the tidyr package in tidyverse
+
+
+
+
+# Load packages ####
+
+library(tidyverse)
+
+
+
+
+# Set up data frame ####
 
 d = data.frame(
-  plotID = 1:4,
-  date = rep(1:2, each=2),
-  daysToEmergance = rep(4:5, each=2),
-  devRate = rep(log(1/4:5), each=2),
-  trt = rep(letters[1:2], each=2),
-  adults = c(3,4,1,2)
+  plotID = rep(1:4, length=8),
+  date = rep(1:4, each=2),
+  daysToEmergance = rep(1:4 + 2, each=2),
+  devRate = rep(log(1/(1:4 + 2)), each=2),
+  trt = rep(letters[1:2], each=2, length=8),
+  adults = c(3,4,1,2,1,2,2,5)
 )
+
+
+
+
+# Repeat rows per number of adults ####
+
+dl = d %>%
+  uncount(adults)
+
+
+
+
+# Compare data frames ####
+
+d
+dl
+# Works!
